a885eec6d55940ccddcc7dfcb0c900ae52943b89
kush <=>
2025-09-05 15:15:15 +0530

    folder issue fix

--- src/middlewares/ImageUploader.js
+++ src/middlewares/ImageUploader.js
@@ -1,11 +1,21 @@
-const multer = require('multer');
+const multer = require("multer");
+const path = require("path");
+
+// storage config
 const storage = multer.diskStorage({
-    destination:(req , file, cb) =>{
-        cb(null , './public/upload');
-    },
-    filename:(req  , file, cb)=>{
-       cb(null , file.originalname);
-    }
+  destination: (req, file, cb) => {
+    const uploadPath =
+      process.env.VERCEL_ENV === "production"
+        ? "/tmp"
+        : path.join(__dirname, "../../public/upload"); // ✅ go 2 levels up
+
+    cb(null, uploadPath);
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
 });
-const ImageUpload = multer({storage:storage})
+
+const ImageUpload = multer({ storage });
 module.exports = ImageUpload;
